0f87cb9ec84a1dd051981a188d50715b30f76dc0
michamoe <98765003+michamoe@users.noreply.github.com>
2023-10-11 15:11:34 +0200

    ready

--- controllers/Pokemon.js
+++ controllers/Pokemon.js
@@ -5,8 +5,6 @@ export const getAllPokemons = async (req, res, next) => {
     const pokemons = await Pokemon.find();
     console.log(pokemons)
     if (!pokemons.length) {
-      //   throw new Error();
-      //   throw { message: 'book not found' };
       throw { statusCode: 404, message: 'pokemon not found' };
     }
     res.json(pokemons);
