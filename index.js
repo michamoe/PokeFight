03576d372e5f26c735d985ddc06ca1696ca42a27
Ajla Saranovic <ge43xed@mytum.de>
2023-10-13 15:09:52 +0200

    aylas-commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import "./db/server.js";
+import mongoose from "mongoose";
+import cors from "cors";
 import pokemonsRouter from "./routes/pokemonsrouters.js";
-
 const app = express();
 const port = 8001;
 
+app.use(cors());
 app.use(express.json());
 app.use("/pokemon", pokemonsRouter);
 
