7b09bbc06c89bfe88c268e576b470051c67132d9
Ajla Saranovic <ge43xed@mytum.de>
2023-10-14 22:25:43 +0200

    new routes

--- routes/pokemonsrouters.js
+++ routes/pokemonsrouters.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import * as pokemonController from '../controllers/pokemon.js';
+import express from "express";
+import * as pokemonController from "../controllers/pokemon.js";
 
 const pokemonsRouter = express.Router();
 
 pokemonsRouter.route("/").get(pokemonController.getAllPokemons);
+pokemonsRouter.route("/:id").get(pokemonController.getPokemonById);
 
 export default pokemonsRouter;
